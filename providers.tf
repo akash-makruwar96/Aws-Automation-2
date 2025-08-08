68a2e956118fe2196b72cbc83b4f5bd6c5d5425c
Akash Makruwar <akashmakruwar1000@gmail.com>
2025-08-08 12:19:10 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region                   = "ap-south-1"
-  shared_credentials_files = ["<Path to your AWS Credentials File>"]
+ # shared_credentials_files = ["<Path to your AWS Credentials File>"]
 
 }
+
